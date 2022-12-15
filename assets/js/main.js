2d8bbdf58649ec9891703a491a8dda7067b90eca
MathildeCornelis <mathilde.cornelis26@gmail.com>
2022-12-15 16:30:00 +0100

    trier tâches js

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,9 @@ document.getElementById('valid').onclick = function validTask() {
 document.querySelector('#createTask').style.visibility = 'hidden';
 };
 
+
+
+
 //quand on valide, les values des inputs s'ajoutent à l'écran
 
     const buttonValid = document.getElementById('valid'); //btn validation  
@@ -29,8 +32,6 @@ document.querySelector('#createTask').style.visibility = 'hidden';
 
     let inputDateValue = inputDate.value; //valeur input date
 
-        inputDate.value = ""
-
         if (textAreaValue == "" || inputDateValue == "" || inputNameValue == "")
 
         {
@@ -41,6 +42,7 @@ document.querySelector('#createTask').style.visibility = 'hidden';
         const tout = document.getElementsByClassName('tout');
         const divTask = document.createElement('div');
         divTask.classList.add("task");
+        
 
         const newh2 = document.createElement('h2');
         newh2.classList.add("name");
@@ -48,7 +50,8 @@ document.querySelector('#createTask').style.visibility = 'hidden';
 
         const newDate = document.createElement('p');
         newDate.classList.add("time");
-        newDate.innerText = inputDateValue;
+        newDate.innerText = tempsRestant();
+        inputDate.value = "";
 
         const newArrow = document.createElement('button');
         newArrow.classList.add("arrow");
@@ -63,7 +66,7 @@ document.querySelector('#createTask').style.visibility = 'hidden';
         newDelete.classList.add('delete');
 
         const newSelect = document.createElement('select');
-        newSelect.id = "status";
+        newSelect.classList.add("status");
 
         const newOption1 = document.createElement('option');
         newOption1.value = 'todo';
@@ -81,7 +84,6 @@ document.querySelector('#createTask').style.visibility = 'hidden';
         newSelect.appendChild(newOption2);
         newSelect.appendChild(newOption3);
 
-        
         divTask.appendChild(newh2);
         divTask.appendChild(newDate);
         divTask.appendChild(newArrow);
@@ -91,3 +93,94 @@ document.querySelector('#createTask').style.visibility = 'hidden';
         tout[0].appendChild(divTask);
         }
     };
+
+    function tempsRestant() {
+        let today = new Date ();
+    
+        let inputDate = document.getElementById('date'); // input date 
+    
+        let inputDateValue = inputDate.value; //valeur input date
+        console.log(inputDate);
+    
+        let futur = new Date(inputDateValue);
+    
+        let result = futur.getTime();
+    
+        let taskTime;
+        if (( Math.floor((result - today.getTime())/ (1000 * 60 * 60 *24))) >= 1)
+        {
+        taskTime = 'Il reste ' +(( Math.floor((result - today.getTime()) / (1000 * 60 * 60 *24))) + 1) + ' jours';}
+            else {
+            taskTime = "Demain"}
+
+            console.log(( Math.floor((result - today.getTime()) / (1000 * 60 * 60 *24))));
+        return taskTime;
+    };
+
+document.getElementById('aFaire').onclick = function () {
+    let done = document.getElementsByClassName('status'); 
+    
+    for (i=0; i < done.length; i++) {
+    let doneValue = done[i].value;
+    if (doneValue != 'todo') {
+        let task = document.getElementsByClassName('task');
+
+                task[i].style.display = 'none'
+        }
+        else {
+            let task = document.getElementsByClassName('task');
+                task[i].style.display = 'flex'
+        };     
+    }
+};
+document.getElementById('Fait').onclick = function () {
+    let done = document.getElementsByClassName('status'); 
+    for (i=0; i < done.length; i++ ) {
+        let doneValue = done[i].value;
+        console.log('test')
+        if (doneValue != 'doing') {
+            let task = document.getElementsByClassName('task');
+
+                task[i].style.display = 'none'
+        }
+        else {
+            let task = document.getElementsByClassName('task');
+                task[i].style.display = 'flex'
+        };     
+    };
+};
+document.getElementById('Fini').onclick = function () {
+    let done = document.getElementsByClassName('status'); 
+    let task
+    for (i=0; i < done.length; i++ ) {
+        let doneValue = done[i].value;
+        console.log(doneValue != 'done')
+        if (doneValue != 'done') {
+            task = document.getElementsByClassName('task');
+        console.log(task[i])
+                task[i].style.display = 'none'
+        }
+        else {
+            task = document.getElementsByClassName('task');
+                task[i].style.display = 'flex'
+        };     
+    };   
+};
+
+// document.getElementById('alphabetique').onclick = function () {
+//     let done = document.getElementsByClassName('status'); 
+//     let task
+//     for (i=0; i < done.length; i++ ) {
+//         let doneValue = done[i].value;
+//         console.log(doneValue != 'done')
+//         if (doneValue != 'done') {
+//             task = document.getElementsByClassName('task');
+//         console.log(task[i])
+//                 task[i].style.display = 'none'
+//         }
+//         else {
+//             task = document.getElementsByClassName('task');
+//                 task[i].style.display = 'flex'
+//         };     
+//     };   
+// };
